4d9a5cfaf305dace00d427280da036948db767a3
Mori <yutomori1979@gmail.com>
2022-12-31 06:05:57 +0900

    メディアクエリを使うために viewport の設定を追加

--- src/components/common/Meta.tsx
+++ src/components/common/Meta.tsx
@@ -16,12 +16,14 @@ export function Meta({ pageTitle, pageDesc }: props) {
   const url = `${siteUrl}${router.asPath}`;
   return (
     <Head>
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="format-detection" content="telephone=no" />
       <meta name="description" content={desc} />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={desc} />
-      <link rel="canonical" href={url} />
       <meta property="og:url" content={url} />
+      <title>{title}</title>
+      <link rel="canonical" href={url} />
     </Head>
   );
 }
